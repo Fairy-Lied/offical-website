3051d0f4da280cfc37a9ffa2b5380475fe8b105c
美兰十三 <luna_yj@hotmail.com>
2026-04-09 13:43:42 +0800

    feat: integrate @vercel/analytics module for analytics tracking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
         }
     },
     devtools: {enabled: true},
-    modules: ['@nuxt/ui', 'nuxt-auth-utils', '@vercel/speed-insights'],
+    modules: ['@nuxt/ui', 'nuxt-auth-utils', '@vercel/speed-insights', '@vercel/analytics'],
     css: ['~/assets/css/main.css'],
 
     // 运行时配置（服务端可用）
